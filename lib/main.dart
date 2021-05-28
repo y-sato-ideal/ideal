38619f3c223ad2ffc3cfda512afcf2177f3f6608
y-sato-ideal <yuuki_sato@ideal-architects.co.jp>
2021-05-28 15:52:15 +0900

    20210528

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:barcode_scan/barcode_scan.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:barcode_scan/barcode_scan.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,25 +11,130 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'QR scaner And QR Maker',
+      title: 'google auth sample',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'QR scaner And QR Maker'),
+      home: AuthPage(
+        title: 'Google Auth Aample with Firebase',
+      ),
+    );
+  }
+}
+
+class AuthPage extends StatefulWidget {
+  AuthPage({
+    Key key,
+    this.title,
+  }) : super(
+          key: key,
+        );
+
+  final String title;
+
+  @override
+  _AuthPageState createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+
+  bool logined = false;
+
+  void login() {
+    setState(() {
+      logined = true;
+    });
+  }
+
+  void logout() {
+    setState(() {
+      logined = false;
+    });
+  }
+
+  Future signInWithGoogle() async {
+    //サインイン画面が表示
+    final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+    final GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount.authentication;
+
+    //firebase側に登録
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleSignInAuthentication.accessToken,
+      idToken: googleSignInAuthentication.idToken,
+    );
+
+    //userのid取得
+    final FirebaseUser user =
+        (await _auth.signInWithCredential(credential)).user;
+
+    assert(!user.isAnonymous);
+    assert(await user.getIdToken() != null);
+
+    final FirebaseUser currentUser = await _auth.currentUser();
+    assert(user.uid == currentUser.uid);
+
+    login();
+
+    await Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (context) {
+        return MyHomePage();
+      }),
+    );
+  }
+
+  //サインアウト
+  void signOutGoogle() async {
+    await googleSignIn.signOut();
+    logout();
+    print("User Sign Out Google");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget logoutText = Text("ログアウト中");
+    Widget loginText = Text("ログイン中");
+
+    Widget loginButton = RaisedButton(
+      child: Text("Sign in Google"),
+      color: Color(0xFFDD4B39),
+      textColor: Colors.white,
+      onPressed: signInWithGoogle,
+    );
+    Widget logoutButton = RaisedButton(
+        child: Text("Sign out"),
+        color: Color(0xFFDD4B39),
+        textColor: Colors.white,
+        onPressed: signOutGoogle);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Google認証'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            logined ? logoutButton : loginButton,
+          ],
+        ),
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
+  MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final GoogleSignIn googleSignIn = GoogleSignIn();
   ScanResult scanResult;
 
   Future _scan() async {
@@ -48,17 +155,13 @@ class _MyHomePageState extends State<MyHomePage> {
       }
       setState(() {
         scanResult = result;
-
-
       });
     }
-    if (scanResult.format?.toString() == "qr" ){
-      Firestore.instance.collection('qrcode').add({
-        "title": scanResult.rawContent
-      });
+    if (scanResult.format?.toString() == "qr") {
+      Firestore.instance
+          .collection('qrcode')
+          .add({"title": scanResult.rawContent});
     }
-
-
   }
 
   @override
@@ -69,38 +172,50 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             children: <Widget>[
               ListTile(
-                title: Text("Result Type"),
+                title: Text("種類"),
                 subtitle: Text(scanResult.type?.toString() ?? ""),
               ),
               ListTile(
-                title: Text("RawContent"),
+                title: Text("取得データ"),
                 subtitle: Text(scanResult.rawContent ?? ""),
               ),
               ListTile(
-                title: Text("Format"),
+                title: Text("形式"),
                 subtitle: Text(scanResult.format?.toString() ?? ""),
               ),
               ListTile(
-                title: Text("Format note"),
-                subtitle: Text(scanResult.formatNote ?? ""),
-              ),
+                title: Text("結果"),
+                subtitle: (scanResult.format?.toString() == "qr")? Text("読み取り出来ました。"): Text("読み取りできませんでした。QRコードを読み取って下さい。"),
+              )
             ],
           ),
         ),
       ListTile(
-
         title: Text("ボタンを押してカメラを起動してください"),
         subtitle: Text("カメラをQRコードに向けてください"),
       ),
-
     ];
-
 
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
           appBar: AppBar(
-            title: Text(widget.title),
+            title: Text('QRコード'),
+            actions: <Widget>[
+              RaisedButton.icon(
+                icon: Icon(Icons.logout),
+                label: Text("sign out"),
+                color: Colors.white60,
+                onPressed: () async {
+                  await googleSignIn.signOut();
+                  await Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context) {
+                      return AuthPage();
+                    }),
+                  );
+                },
+              )
+            ],
           ),
           body: ListView(
             scrollDirection: Axis.vertical,
@@ -112,13 +227,6 @@ class _MyHomePageState extends State<MyHomePage> {
             tooltip: 'Scan',
             child: Icon(Icons.camera),
           ),
-
         ));
-
-
   }
-
-
-
-
 }
